dd60eaeaed941c6604a075aa3293e0aece8ee4f0
eransam <eransam21@gmail.com>
2022-06-28 20:38:46 +0300

    TEst-2

--- eran.js
+++ eran.js
@@ -1,8 +1,9 @@
 asdad'ajlfba'lkfa'
 as;djbasd
 as'jbhasd
-a'slkda'SharedWorkerasd'lkhasd
+
+
 dsds
-s;kshskpshpshspohsopshsposhpshsphspsh
+s;kshskpshpshspohsopshsposhpshsphspshERAN22222
 sp[ishpishsphi]shspishishsis
 s[oshslhsksh]
